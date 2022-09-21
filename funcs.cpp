140f573d15e3098e2f7aaffcd1f2c1d95c20edad
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-09-21 19:42:23 -0400

    task E done

--- funcs.cpp
+++ funcs.cpp
@@ -107,7 +107,25 @@ std::string lower(int length){
   }
   return 0;
 }
-      
+
+std::string upper(int length){
+
+  std::string star = "*";
+  std::string space = " ";
+
+  for (int row = 0; row < length; row++) {
+    for (int col = 0; col < length; col++) {
+      if (col >= row){
+	std::cout << star;
+      }
+      else {
+	std::cout << space;
+      }
+    }
+    std::cout << std::endl;
+  }
+  return 0;
+}
   
 
   
