f2ae3fe2fdf146007be3fc67c9c0fc953de31f0d
Natalie Gallo <nataliegallo@Natalies-Air.fios-router.home>
2022-09-21 19:36:00 -0400

    task d complete

--- funcs.cpp
+++ funcs.cpp
@@ -96,7 +96,7 @@ std::string lower(int length){
 
   for (int row = 0; row <= length; row++) {
     for (int col = 0; col <= length; col++) {
-      if (col > row){
+      if (col < row){
 	std::cout << star;
       }
       else {
